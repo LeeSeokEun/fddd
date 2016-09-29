f2e98072e3bbe52761eef1c8df2a4b79d1a04f8f
이석은 <skfn7979@naver.com>
2016-09-30 03:27:12 +0900

    9/30 오전 3시 27분

--- app/src/main/java/com/example/leeseokeun/fddd/MainActivity.java
+++ app/src/main/java/com/example/leeseokeun/fddd/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     private int sampleRate = 8000;
     private int inputBlockSize = 256;
     private int sampleDecimate = 1;
-    int temp = 0;
-    String str =  String.valueOf(temp);
+    public int temp;
+    //String str =  String.valueOf(temp);
     //String str = String.format("%d",temp);
     TextView CCC;
 
@@ -28,9 +28,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         CCC = (TextView) findViewById(R.id.test);
-        CCC.setText(str);
+        //CCC.setText(str);
+        CCC.setText(String.format("현재 데시벨은 %d 입니다",temp));
         audioReader = new AudioReader();
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
@@ -61,12 +61,11 @@ public class MainActivity extends AppCompatActivity {
     // 0에 가까운 숫자를 더해서 데시벨을 양수측으로 움직임
     // 기본 평소 대화소리를 40~60dB로 가정
 
-    public void receiveDecibel(final int dB)
+    public  void receiveDecibel(final int dB)
     {
         Log.e("###", dB+90 +" dB");
         temp = dB;
-        //Log.e("###", temp+90 +" temp");
-
+        Log.e("###", temp+90 +" temp"); // temp에 값이 정확히 들어가는지 확인.
     }
 
     @Override
